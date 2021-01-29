11a130e994b1b4050142f0a7cb7a1ec9c6556388
clf94-dev <clf94@hotmail.es>
2021-01-29 17:01:20 +0100

    added style frameworks

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -10,16 +10,16 @@ function Frameworks(){
         <div className="frameworks-cont">
             <h3>Projects by framework</h3>
             <div className="react-cont">
-                <h3>React   <i className='fab fa-4x fa-react react'></i></h3>
-                <p>Also used with React:  <i className='fab fa-4x fa-sass sass'></i>,<i className="fab fa-4x fa-font-awesome"></i>, Material-UI, AOS Animation </p>
+                <h3>React   <i className='fab fa-2x fa-react react'></i></h3>
+                <p>Also used with React:  <i className='fab fa-sass sass'></i>,<i className="fab fa-font-awesome font-awe"></i>, Material-UI, AOS Animation </p>
             </div>
             <div className="angular-cont">
-                <h3>Angular   <i className='fab fa-4x fa-angular angular'></i></h3>
-                <p>Also used with Angular:  <i className='fab fa-4x fa-sass sass'></i>,<i className="fab fa-4x fa-font-awesome"></i>, Flex-Layout, AOS Animation </p>
+                <h3>Angular   <i className='fab fa-2x fa-angular angular'></i></h3>
+                <p>Also used with Angular:  <i className='fab fa-sass sass'></i>,<i className="fab fa-font-awesome font-awe"></i>, Flex-Layout, AOS Animation </p>
             </div>
             <div className="vue-cont">
-                <h3>Vue   <i className='fab fa-4x fa-vuejs vue'></i></h3>
-                <p>Also used with Vue:    <i className='fab fa-4x fa-css3-alt css'></i>,<i className="fab fa-4x fa-font-awesome"></i>,<i className="fab fa-4x fa-bootstrap boots"></i>,vue-router, AOS Animation </p>
+                <h3>Vue   <i className='fab fa-2x fa-vuejs vue'></i></h3>
+                <p>Also used with Vue:    <i className='fab fa-css3-alt css'></i>,<i className="fab fa-font-awesome font-awe"></i>,<i className="fab fa-bootstrap boots"></i>,vue-router, AOS Animation </p>
             </div>
         </div>
     )}
