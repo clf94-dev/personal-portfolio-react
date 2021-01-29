593c59ba2af339be9a6784e881c5a30d8ef0f5bb
clf94-dev <clf94@hotmail.es>
2021-01-29 17:16:40 +0100

    added links frameworks

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Grid} from '@material-ui/core/';
-
+import {Link} from 'react-router-dom';
 import {Button} from './Button';
 import './styles/App.css';
 
@@ -9,18 +9,18 @@ function Frameworks(){
     return(
         <div className="frameworks-cont">
             <h3>Projects by framework</h3>
-            <div className="react-cont">
+           <Link className="link" to='/portfolioreact'> <div className="react-cont" onClick="">
                 <h3>React   <i className='fab fa-2x fa-react react'></i></h3>
                 <p>Also used with React:  <i className='fab fa-sass sass'></i>,<i className="fab fa-font-awesome font-awe"></i>, Material-UI, AOS Animation </p>
-            </div>
-            <div className="angular-cont">
+            </div></Link>
+            <Link className="link" to='/portfolioangular'><div className="angular-cont">
                 <h3>Angular   <i className='fab fa-2x fa-angular angular'></i></h3>
                 <p>Also used with Angular:  <i className='fab fa-sass sass'></i>,<i className="fab fa-font-awesome font-awe"></i>, Flex-Layout, AOS Animation </p>
-            </div>
-            <div className="vue-cont">
+            </div></Link>
+            <Link className="link" to='/portfoliovue'><div className="vue-cont">
                 <h3>Vue   <i className='fab fa-2x fa-vuejs vue'></i></h3>
                 <p>Also used with Vue:    <i className='fab fa-css3-alt css'></i>,<i className="fab fa-font-awesome font-awe"></i>,<i className="fab fa-bootstrap boots"></i>,vue-router, AOS Animation </p>
-            </div>
+            </div></Link>
         </div>
     )}
 
