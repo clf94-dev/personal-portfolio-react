bb32d7db0269108c5fc1f5f49ea22b5dd268d479
clf94-dev <clf94@hotmail.es>
2021-01-29 16:34:24 +0100

    added frameworks projects component

---
+++ src/components/Frameworks.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Grid} from '@material-ui/core/';
+
+import {Button} from './Button';
+import './styles/App.css';
+
+
+function Frameworks(){
+    return(
+        <div className="frameworks-cont">
+            <h3>Projects by framework</h3>
+            <div className="react-cont">
+                <h3>React   <i className='fab fa-4x fa-react react'></i></h3>
+                <p>Also used with React:  <i className='fab fa-4x fa-sass sass'></i>,<i className="fab fa-4x fa-font-awesome"></i>, Material-UI, AOS Animation </p>
+            </div>
+            <div className="angular-cont">
+                <h3>Angular   <i className='fab fa-4x fa-angular angular'></i></h3>
+                <p>Also used with Angular:  <i className='fab fa-4x fa-sass sass'></i>,<i className="fab fa-4x fa-font-awesome"></i>, Flex-Layout, AOS Animation </p>
+            </div>
+            <div className="vue-cont">
+                <h3>Vue   <i className='fab fa-4x fa-vuejs vue'></i></h3>
+                <p>Also used with Vue:    <i className='fab fa-4x fa-css3-alt css'></i>,<i className="fab fa-4x fa-font-awesome"></i>,<i className="fab fa-4x fa-bootstrap boots"></i>,vue-router, AOS Animation </p>
+            </div>
+        </div>
+    )}
+
+export default Frameworks;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,13 +5,14 @@ import Languages from '../Languages'
 import '../styles/App.css';
 import AboutSection from '../AboutSection';
 import Navbar from '../Navbar';
+import Frameworks from '../Frameworks';
 
 export default function Home() {
 
     useEffect(() => {
         window.scrollTo(0, 0)
       }, []);
-    return ( < div > <Navbar active='home'/> < HeroSection / > <AboutSection/> <Languages/> < Cta /> </div>);
+    return ( < div > <Navbar active='home'/> < HeroSection / > <AboutSection/> <Languages/> <Frameworks/> < Cta /> </div>);
 }
 
  
